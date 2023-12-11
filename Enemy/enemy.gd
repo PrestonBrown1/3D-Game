3117d7b897a6ce268a36d77cb925e95d0b19205b
PrestonBrown1 <browpr@iu.edu>
2023-12-10 20:26:17 -0500

    Enemy moves toward player

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 2.5
 const JUMP_VELOCITY = 4.5
 var player
 
@@ -31,8 +31,28 @@ func _physics_process(delta):
 		direction += deg_to_rad(180)
 
 	rotation.y = direction - deg_to_rad(90)
-	print(rad_to_deg(direction))
-	#velocity.z = SPEED
 	
+	#Calculate Velocity
+	var inputDir = Vector2()
+	if rotation.y > 0:
+		inputDir.x = 1
+		#velocity.x = SPEED
+	else:
+		inputDir.x = -1
+		#velocity.x = SPEED * -1
+	if abs(rad_to_deg(rotation.y)) > 90:
+		inputDir.y = 1
+		#velocity.z = SPEED * -1
+	else:
+		inputDir.y = 1
+		#velocity.z = SPEED
+	print(inputDir)
+	var angle = (transform.basis * Vector3(inputDir.x, 0, inputDir.y)).normalized()
+	if angle:
+		velocity.x = angle.x * SPEED
+		velocity.z = angle.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
